55efd140f5fc7080053b7bc78e3d97b18d68566f
Samuel Sarle <samuel@asimov.systems>
2025-06-27 20:47:01 +0300

    Add optional flag for selecting module

--- src/commands/import.rs
+++ src/commands/import.rs
@@ -1,13 +1,22 @@
 // This is free and unencumbered software released into the public domain.
 
-use crate::{commands::External, shared::build_resolver, StandardOptions, SysexitsError};
+use crate::{
+    commands::External,
+    shared::build_resolver,
+    StandardOptions,
+    SysexitsError::{self, *},
+};
 use color_print::ceprintln;
 use miette::Result;
 
-pub fn import(urls: &Vec<String>, flags: &StandardOptions) -> Result<(), SysexitsError> {
+pub fn import(
+    urls: &Vec<String>,
+    module: Option<&str>,
+    flags: &StandardOptions,
+) -> Result<(), SysexitsError> {
     let resolver = build_resolver("importer").map_err(|e| {
         ceprintln!("<s,r>error:</> failed to build a resolver: {e}");
-        SysexitsError::EX_UNAVAILABLE
+        EX_UNAVAILABLE
     })?;
 
     for url in urls {
@@ -17,29 +26,30 @@ pub fn import(urls: &Vec<String>, flags: &StandardOptions) -> Result<(), Sysexit
 
         let modules = resolver.resolve(url)?;
 
-        match modules.first() {
-            Some(module) => {
-                let subcommand = format!("{}-importer", module.name);
+        let module = if let Some(want) = module {
+            modules.iter().find(|m| m.name == want).ok_or_else(|| {
+                ceprintln!("<s,r>error:</> failed to find a module named `{want}` that supports importing the URL: `{url}`");
+                EX_SOFTWARE
+            })?
+        } else {
+            modules.first().ok_or_else(|| {
+                ceprintln!("<s,r>error:</> failed to find a module to import the URL: `{url}`");
+                EX_SOFTWARE
+            })?
+        };
 
-                let cmd = External {
-                    is_debug: flags.debug,
-                    pipe_output: false,
-                };
+        let subcommand = format!("{}-importer", module.name);
 
-                let code = cmd
-                    .execute(&subcommand, &[url.to_owned()])
-                    .map(|result| result.code)?;
-                if code.is_failure() {
-                    return Err(code);
-                }
-            }
-            None => {
-                ceprintln!(
-                    "<s,r>error:</> failed to find a module to import the URL: `{}`",
-                    url
-                );
-                return Err(SysexitsError::EX_SOFTWARE);
-            }
+        let cmd = External {
+            is_debug: flags.debug,
+            pipe_output: false,
+        };
+
+        let code = cmd
+            .execute(&subcommand, &[url.to_owned()])
+            .map(|result| result.code)?;
+        if code.is_failure() {
+            return Err(code);
         }
 
         if flags.verbose > 0 {
