679d0fe61d7f43ab740ae75c1b910c15621e23bb
Samuel Sarle <samuel@asimov.systems>
2025-09-18 14:28:52 +0300

    Define `crate::shared::installed_modules` helper

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -3,8 +3,9 @@
 use crate::{
     StandardOptions,
     SysexitsError::{self, *},
+    shared,
 };
-use asimov_module::{ModuleManifest, resolve::Resolver, url::normalize_url};
+use asimov_module::{resolve::Resolver, url::normalize_url};
 use asimov_runner::{CatalogerOptions, GraphOutput};
 use color_print::ceprintln;
 use miette::Result;
@@ -17,33 +18,7 @@ pub async fn list(
     flags: &StandardOptions,
 ) -> Result<(), SysexitsError> {
     let registry = asimov_registry::Registry::default();
-    let installed_modules: Vec<ModuleManifest> = registry
-        .installed_modules()
-        .await
-        .map_err(|e| {
-            ceprintln!("<s,r>error:</> unable to access installed modules: {e}");
-            match e {
-                asimov_registry::error::InstalledModulesError::DirIo(_, err)
-                    if err.kind() == std::io::ErrorKind::NotFound => {
-                        ceprintln!("<s,dim>hint:</> There appears to be no installed modules.");
-                        ceprintln!("<s,dim>hint:</> Modules may be discovered either on the site <u>https://asimov.directory/modules</>");
-                        ceprintln!("<s,dim>hint:</> or on the GitHub organization <u>https://github.com/asimov-modules</>");
-                        ceprintln!("<s,dim>hint:</> and installed with `asimov module install <<module>>`");
-                    },
-                _ => (),
-            };
-            EX_UNAVAILABLE
-        })?
-        .into_iter()
-        .map(|manifest| manifest.manifest)
-        .filter(|manifest| {
-            manifest
-                .provides
-                .programs
-                .iter()
-                .any(|program| program.ends_with("-cataloger"))
-        })
-        .collect();
+    let installed_modules = shared::installed_modules(&registry, Some("cataloger")).await?;
 
     let resolver = Resolver::try_from_iter(installed_modules.iter()).map_err(|e| {
         ceprintln!("<s,r>error:</> failed to build resolver: {e}");
--- src/shared.rs
+++ src/shared.rs
@@ -1,7 +1,12 @@
 // This is free and unencumbered software released into the public domain.
 
 use crate::Result;
-use clientele::{Subcommand, SubcommandsProvider, SysexitsError::*};
+use asimov_module::ModuleManifest;
+use clientele::{
+    Subcommand, SubcommandsProvider,
+    SysexitsError::{self, *},
+};
+use color_print::{ceprintln, cstr};
 
 /// Locates the given subcommand or prints an error.
 pub fn locate_subcommand(name: &str) -> Result<Subcommand> {
@@ -12,4 +17,45 @@ pub fn locate_subcommand(name: &str) -> Result<Subcommand> {
             Err(EX_UNAVAILABLE)
         },
     }
+}
+
+const NO_MODULES_FOUND_HINT: &str = cstr!(
+    r#"<s,dim>hint:</> There appears to be no installed modules.
+<s,dim>hint:</> Modules may be discovered either on the site <u>https://asimov.directory/modules</>
+<s,dim>hint:</> or on the GitHub organization <u>https://github.com/asimov-modules</>
+<s,dim>hint:</> and installed with <s>asimov module install <<module>></>"#
+);
+
+pub async fn installed_modules(
+    registry: &asimov_registry::Registry,
+    filter: Option<&str>,
+) -> Result<Vec<ModuleManifest>, SysexitsError> {
+    let modules = registry
+        .installed_modules()
+        .await
+        .map_err(|e| {
+            ceprintln!("<s,r>error:</> unable to access installed modules: {e}");
+            if let asimov_registry::error::InstalledModulesError::DirIo(_, err) = e
+                && err.kind() == std::io::ErrorKind::NotFound
+            {
+                ceprintln!("{NO_MODULES_FOUND_HINT}");
+            }
+            EX_UNAVAILABLE
+        })?
+        .into_iter()
+        .map(|manifest| manifest.manifest)
+        .filter(|manifest| {
+            if let Some(filter) = filter {
+                manifest
+                    .provides
+                    .programs
+                    .iter()
+                    .any(|program| program.split("-").last().is_some_and(|p| p == filter))
+            } else {
+                true
+            }
+        })
+        .collect();
+
+    Ok(modules)
 }
