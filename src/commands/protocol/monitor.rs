756e486ab11781751eb0c0bc97a6be81fff0de88
Arto Bendiken <arto@asimov.systems>
2026-07-03 01:10:05 -0700

    Implement `asimov protocol {ping,monitor}`.

--- src/commands/protocol/monitor.rs
+++ src/commands/protocol/monitor.rs
@@ -1,7 +1,25 @@
 // This is free and unencumbered software released into the public domain.
 
 use crate::{StandardOptions, SysexitsError};
+use asimov_protocol::{DefaultPreset, Endpoint, EndpointTicket, PING_ALPN, PingProtocol, Router};
 
 pub async fn monitor(_flags: &StandardOptions) -> Result<(), SysexitsError> {
-    Ok(()) // TODO
+    // Create an endpoint and accept connections from peers:
+    let self_endpoint = Endpoint::bind(DefaultPreset).await.unwrap();
+    self_endpoint.online().await;
+
+    // Start the ping protocol service router:
+    let service = PingProtocol::new();
+    let _router = Router::builder(self_endpoint.clone())
+        .accept(PING_ALPN, service)
+        .spawn();
+
+    // Print out the endpoint's ticket that allows connecting to it:
+    let self_ticket = EndpointTicket::new(self_endpoint.addr());
+    std::println!("{}", self_ticket);
+
+    // Wait until the user presses Ctrl-C to terminate the program:
+    tokio::signal::ctrl_c().await?;
+
+    Ok(())
 }
--- src/commands/protocol/ping.rs
+++ src/commands/protocol/ping.rs
@@ -1,10 +1,22 @@
 // This is free and unencumbered software released into the public domain.
 
 use crate::{StandardOptions, SysexitsError};
+use asimov_protocol::{DefaultPreset, Endpoint, EndpointTicket, PingProtocol, Ticket};
 
-pub async fn ping(
-    _recipient: impl AsRef<str>,
-    _flags: &StandardOptions,
-) -> Result<(), SysexitsError> {
-    Ok(()) // TODO
+pub async fn ping(ticket: impl AsRef<str>, _flags: &StandardOptions) -> Result<(), SysexitsError> {
+    // Parse the peer ticket provided by the user:
+    let peer_ticket = EndpointTicket::decode_string(ticket.as_ref()).unwrap();
+
+    // Connect to the remote peer endpoint:
+    let self_endpoint = Endpoint::bind(DefaultPreset).await.unwrap();
+    let service = PingProtocol::new();
+    let rtt = service
+        .ping(&self_endpoint, peer_ticket.endpoint_addr().clone())
+        .await?;
+
+    std::eprintln!("Pinged the peer in {rtt:?} RTT");
+
+    self_endpoint.close().await;
+
+    Ok(())
 }
