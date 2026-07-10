22abf5255d4c09b6e3b677ec2df7b3603e5e1a0b
Arto Bendiken <arto@asimov.systems>
2026-07-10 05:46:25 -0700

    Implement `asimov handle list`.

--- src/commands/handle/list.rs
+++ src/commands/handle/list.rs
@@ -1,8 +1,19 @@
 // This is free and unencumbered software released into the public domain.
 
 use crate::StandardOptions;
+use asimov_directory::fs::HandleResolver;
+use color_print::ceprintln;
 use core::error::Error;
+use futures_lite::{pin, stream::StreamExt};
 
 pub async fn list(_flags: &StandardOptions) -> Result<(), Box<dyn Error>> {
-    Ok(()) // TODO
+    let mut resolver = HandleResolver::default().await?;
+    let handles = resolver.handles();
+    pin!(handles);
+
+    while let Some(handle) = handles.next().await {
+        ceprintln!("{}", handle?);
+    }
+
+    Ok(())
 }
